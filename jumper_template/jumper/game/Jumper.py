38608c33d1c84a20b9922b521c0d4e187e877564
Mototoe <bradnon4cool@gmail.com>
2021-05-21 14:10:11 -0600

    Updated Jumper

--- jumper_template/jumper/game/Jumper.py
+++ jumper_template/jumper/game/Jumper.py
@@ -6,5 +6,37 @@
 class Jumper:
     
     
-    def __init__(self):
-        pass
+    def get_parachute(self):
+        '''Draw Jumper and subtract Lines for loss of life'''
+        self.lives = 4
+    
+        message =  (" __ \n" +
+
+                    "/__\ \n" +
+
+                    "\  / \n" +
+
+                    " \/ \n" +
+
+                    "  0 \n" +
+
+                    " /|\ \n" +
+
+                    " / \ \n" +
+
+                      "\n" +
+
+                    "^^^^^^^^ \n")
+        return message
+
+       
+
+    def cutline(self, lives,message):
+        '''remove lines from jumper based on lives left'''
+        if lives == 4:
+            pass
+
+
+
+        
+        
